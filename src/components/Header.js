64556dd520a23c71cce4e404b9afc7ff1d6b6052
studmanj <bjackso3@hotmail.com>
2022-10-16 01:35:59 -0600

    Header component

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+
+function Header() {
+    return(
+        <header class="header">
+            <img className="header--image" 
+                 src="./images/troll-face.png" 
+                 alt="Meme troll-face" />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </header>
+    )
+}
+
+export default Header
